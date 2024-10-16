b62108fe75c452521ddd66de30d9d18cf1d56f9e
Mingzhe Li <mingzhe.helian@gmail.com>
2024-10-16 14:53:27 +1100

    feat(app): add tasks component

---
+++ attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { type Task } from './task.model';
+
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  task = input.required<Task>();
+}
--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output, output, signal } from '@angular/core';
+import { type User } from './user.model';
+
 
 @Component({
   selector: 'app-user',
@@ -10,16 +12,24 @@ import { Component, computed, input, Input, signal } from '@angular/core';
 export class UserComponent {
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
+  // @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(()=> {return `assets/users/${this.avatar}`})
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // id = input.required<string>();
+
+  user = input.required<User>();
+
+  select = output<string>();
+
+  imagePath = computed(() => {return `assets/users/${this.user().avatar}`})
 
   // get imagePath() {
   //   return `assets/users/${this.avatar}`;
   // }
 
   onSelectUser() {
+    this.select.emit(this.user().id);
   }
 
 }
